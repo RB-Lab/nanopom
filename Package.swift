af3bfb02dec567bb8096bfff7312e56fdecc34a7
Roman Bekkiev <rb@rblab.net>
2024-08-04 16:07:24 +0200

    setup for packaging

--- Package.swift
+++ Package.swift
@@ -4,22 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "nanopom",
-    platforms: [
-        .macOS(.v11)
-    ],
-    dependencies: [],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "nanopom",
-            dependencies: [],
-            path: "Sources",
-            exclude: ["Info.plist"],
-            swiftSettings: [
-                .unsafeFlags(["-Xlinker", "-sectcreate", "-Xlinker", "__TEXT", "-Xlinker", "__info_plist", "-Xlinker", "Info.plist"])
-            ]
-        )
-    ]
+  name: "nanopom",
+  platforms: [
+    .macOS(.v11)
+  ],
+  dependencies: [],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "NanoPomodoro",
+      dependencies: [],
+      path: "Sources",
+      exclude: ["Info.plist"],
+      swiftSettings: [
+        .unsafeFlags([
+          "-Xlinker", "-sectcreate", "-Xlinker", "__TEXT", "-Xlinker", "__info_plist", "-Xlinker",
+          "Info.plist",
+        ])
+      ]
+    )
+  ]
 )
